1a47ffcb005e0ca6383fbce342da5b4adc2639b6
Mario Andrés Mercado Coronado <mariomerco@Marios-MacBook-Pro.local>
2020-04-25 13:16:24 -0500

    dev and prod environments for the front end

---
+++ front-end/src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  resourceApi: "https://api.dev.mariomerco.com/",
+  inventoryApi: "https://api.dev.mariomerco.com/",
+  clientApi: "https://api.dev.mariomerco.com/",
+  rentingApi: "https://api.dev.mariomerco.com/" 
+};
