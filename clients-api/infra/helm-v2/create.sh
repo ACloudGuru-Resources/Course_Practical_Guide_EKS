f6029becb978e016ab6ff15f3b34b8cf352e7ca4
Mario Andrés Mercado Coronado <mariomerco@Marios-MacBook-Pro.local>
2020-05-17 17:58:42 -0500

    create command of the IAM helm chart should only be the upgrade

--- clients-api/infra/helm-v2/create.sh
+++ clients-api/infra/helm-v2/create.sh
@@ -1,8 +1,1 @@
-eksctl create iamserviceaccount \
-    --cluster eks-acg \
-    --name clients-api-iam-service-account \
-    --namespace development \
-    --attach-policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess \
-    --approve --override-existing-serviceaccounts
-
 helm upgrade --install --namespace development clients-api-development .
--- resource-api/infra/helm-v2/create.sh
+++ resource-api/infra/helm-v2/create.sh
@@ -1,8 +1,1 @@
-eksctl create iamserviceaccount \
-    --cluster eks-acg \
-    --name resources-api-iam-service-account \
-    --namespace development \
-    --attach-policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess \
-    --approve --override-existing-serviceaccounts
-
 helm upgrade --install --namespace development resource-api-development .
