907807d0c0fee7b7dfc237a0318750bc9d8f0fad
Mario Andrés Mercado Coronado <mariomerco@Marios-MacBook-Pro.local>
2020-05-23 21:06:00 -0500

    solving CORS issues

--- renting-api/api/config/environments/development.rb
+++ renting-api/api/config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.hosts << "renting-api"
+  config.hosts << "renting-api-service"
   config.cache_classes = false
 
   # Do not eager load code on boot.
