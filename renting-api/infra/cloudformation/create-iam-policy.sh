25674c0b7316fca551789262193f5a8480fbd045
Mario Andrés Mercado Coronado <mariomerco@Marios-MacBook-Pro-16.local>
2020-07-28 07:46:26 -0500

    create dynamodb and iam poliocy scripts

---
+++ renting-api/infra/cloudformation/create-iam-policy.sh
@@ -0,0 +1,14 @@
+namespace=$1
+
+if [ -z "${namespace}" ]
+then
+    echo "Missing the 'Namespace' parameter. Taking the default one which is 'development'"
+    namespace="development"
+fi
+
+aws cloudformation deploy \
+    --stack-name ${namespace}-iam-policy-renting-api \
+    --template-file iam-policy.json \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+        Namespace=${namespace}
