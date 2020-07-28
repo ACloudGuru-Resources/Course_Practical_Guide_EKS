62108d8ca9e8c8c6fc18520bf8b26f5160afabc9
Mario Andrés Mercado Coronado <mariomerco@Marios-MacBook-Pro-16.local>
2020-07-28 08:26:22 -0500

    was missing updating the chart for the front end

--- front-end/infra/helm/create.sh
+++ front-end/infra/helm/create.sh
@@ -1,1 +1,9 @@
-helm upgrade --install --namespace development front-end-development .
+namespace=$1
+
+if [ -z "${namespace}" ]
+then
+    echo "Missing the 'Namespace' parameter. Taking the default one which is 'development'"
+    namespace="development"
+fi
+
+helm upgrade --install --namespace ${namespace} -f values.yaml -f values.${namespace}.yaml front-end-${namespace} .
