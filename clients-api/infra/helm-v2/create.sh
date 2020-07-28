e2d3d1726320f0be4c9ac54d5338c3bca3f9035a
Mario Andrés Mercado Coronado <mariomerco@Marios-MacBook-Pro-16.local>
2020-07-28 08:19:10 -0500

    create.sh for helm-v2 now supports namespace parameter

--- clients-api/infra/helm-v2/create.sh
+++ clients-api/infra/helm-v2/create.sh
@@ -1,1 +1,9 @@
-helm upgrade --install --namespace development clients-api-development .
+namespace=$1
+
+if [ -z "${namespace}" ]
+then
+    echo "Missing the 'Namespace' parameter. Taking the default one which is 'development'"
+    namespace="development"
+fi
+
+helm upgrade --install --namespace ${namespace} clients-api-${namespace} .
