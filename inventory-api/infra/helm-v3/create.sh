efcaef56a92fe27c508f5ec1e9e25a4252196354
Mario Andrés Mercado Coronado <mariomerco@Marios-MacBook-Pro-16.local>
2020-08-08 16:44:41 -0500

    inventory api mesh

---
+++ inventory-api/infra/helm-v3/create.sh
@@ -0,0 +1,9 @@
+namespace=$1
+
+if [ -z "${namespace}" ]
+then
+    echo "Missing the 'Namespace' parameter. Taking the default one which is 'development'"
+    namespace="development"
+fi
+
+helm upgrade --install --namespace ${namespace} inventory-api-${namespace} .
