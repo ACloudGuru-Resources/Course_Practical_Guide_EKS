37588d7158a882c7c32119afbaeae2149fa1f169
mariomerco <mario.mercado.c@gmail.com>
2022-11-19 17:11:29 -0500

    reordered

--- Infrastructure/k8s-tooling/2-alb-controller/create.sh
+++ Infrastructure/k8s-tooling/2-alb-controller/create.sh
@@ -1,1 +1,4 @@
-kubectl apply -f alb-ingress-controller.yaml
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
+  -n kube-system \
+  --set clusterName=eks-acg \
+  --set serviceAccount.create=true
