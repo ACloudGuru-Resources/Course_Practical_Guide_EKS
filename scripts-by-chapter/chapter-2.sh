eb5aa9654ce91c52e9ff3016f742ad0f65a38f3d
mariomerco <mario.mercado.c@gmail.com>
2023-02-22 18:06:12 -0500

    uploading for AWS CloudShell

---
+++ scripts-by-chapter/chapter-2.sh
@@ -0,0 +1,20 @@
+#  Create the DynamoDB Tables
+    ( cd ./clients-api/infra/cloudformation && ./create-dynamodb-table.sh development )
+    ( cd ./inventory-api/infra/cloudformation && ./create-dynamodb-table.sh development )
+    ( cd ./renting-api/infra/cloudformation && ./create-dynamodb-table.sh development )
+    ( cd ./resource-api/infra/cloudformation && ./create-dynamodb-table.sh development )
+
+# Create the Kubernetes Cluster
+    eksctl create cluster -f Infrastructure/eksctl/01-initial-cluster/cluster.yaml
+    nodegroup_iam_role=$(aws cloudformation list-exports --query "Exports[?contains(Name, 'nodegroup-eks-node-group::InstanceRoleARN')].Value" --output text | xargs | cut -d "/" -f 2)
+
+# Adding DynamoDB Permissions to the node
+    aws iam attach-role-policy --role-name ${nodegroup_iam_role} --policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess
+
+# Installing ExternalDNS
+    ./Infrastructure/k8s-tooling/1-external-dns/create.sh
+    aws iam attach-role-policy --role-name ${nodegroup_iam_role} --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess
+
+# Installing Load Balancer Controller
+    ./Infrastructure/k8s-tooling/2-load-balancer-controller/create.sh
+    aws iam attach-role-policy --role-name ${nodegroup_iam_role} --policy-arn arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess
