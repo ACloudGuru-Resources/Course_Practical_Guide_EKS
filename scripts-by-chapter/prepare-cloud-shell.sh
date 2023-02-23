2fe9e81a50318cf4c1cd80c7db46595e74296090
mariomerco <mario.mercado.c@gmail.com>
2023-02-23 18:03:10 -0500

    preparing cloudshell

---
+++ scripts-by-chapter/prepare-cloud-shell.sh
@@ -0,0 +1,9 @@
+echo "---------- INSTALLING EKSCTL "----------"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+
+echo "---------- INSTALLING HELM "----------"
+export VERIFY_CHECKSUM=false
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
