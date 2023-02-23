f34d8e5c05bcfc742a70f5fd713e1930fbc6e9ed
mariomerco <mario.mercado.c@gmail.com>
2023-02-23 18:35:35 -0500

    k8s v1.25

--- Infrastructure/k8s-tooling/2-load-balancer-controller/create.sh
+++ Infrastructure/k8s-tooling/2-load-balancer-controller/create.sh
@@ -1,3 +1,4 @@
+helm repo add eks https://aws.github.io/eks-charts
 helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
   -n kube-system \
   --set clusterName=eks-acg \
--- scripts-by-chapter/prepare-cloud-shell.sh
+++ scripts-by-chapter/prepare-cloud-shell.sh
@@ -1,9 +1,12 @@
-echo "---------- INSTALLING EKSCTL "----------"
+echo "---------- INSTALLING NANO ----------"
+sudo yum install nano -y
+
+echo "---------- INSTALLING EKSCTL ----------"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
 
-echo "---------- INSTALLING HELM "----------"
+echo "---------- INSTALLING HELM ----------"
 export VERIFY_CHECKSUM=false
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
